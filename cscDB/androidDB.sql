a19f1c449a494a7d5c8c929b5c932d9ed3038907
RubenTadeia <rubent03@gmail.com>
2016-12-20 10:23:23 +0000

    Código para base de dados

---
+++ cscDB/androidDB.sql
@@ -0,0 +1,32 @@
+USE ist175268;
+
+SET FOREIGN_KEY_CHECKS = 0; /* Disable foreign key checking.*/
+drop table if exists child;
+drop table if exists parent;
+drop table if exists controls;
+SET FOREIGN_KEY_CHECKS = 1; /* Enable foreign key checking.*/
+
+
+create table IF NOT EXISTS child(
+	childNumber integer NOT NULL, /* Numero de Telefone da criança */
+    name varchar(30) NOT NULL,
+    email integer NOT NULL,
+    primary key(childNumber)
+    );
+
+
+create table IF NOT EXISTS parent(
+	parentNumber integer NOT NULL, /* Numero de Telefone do responsavel */
+    name varchar(30) NOT NULL,
+    email integer NOT NULL,
+    primary key(parentNumber)
+    );
+
+
+create table IF NOT EXISTS controls(
+	childNumber integer NOT NULL,
+   	parentNumber integer NOT NULL,
+    primary key(childNumber, parentNumber),
+    foreign key(childNumber) references child(childNumber),
+    foreign key(parentNumber) references parent(parentNumber)
+    );
