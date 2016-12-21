9b7707dbb72d6a77bfb7a328ae81075fd1361f1f
RubenTadeia <rubent03@gmail.com>
2016-12-21 22:45:28 +0000

    ran unzip versao 2 up - sem apk

---
+++ Projecto/CSC_versao1/app/src/main/java/brunoc/csc/ist/pt/csc/brunoc/csc/ist/pt/csc/utils/OnFragmentInteractionListener.java
@@ -0,0 +1,11 @@
+package brunoc.csc.ist.pt.csc.brunoc.csc.ist.pt.csc.utils;
+
+import android.net.Uri;
+
+/**
+ * Created by Bruno on 10-11-2016.
+ */
+
+public interface OnFragmentInteractionListener {
+    void onFragmentInteraction(Uri uri);
+}
